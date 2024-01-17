09266a73c8166dadcca20c5d6563a91a565523d7
mvykool <maykolandres4@gmail.com>
2024-01-17 06:35:58 -0500

    added a few things

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ToolbarModule } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
+import { TabViewModule } from 'primeng/tabview';
 @Component({
 	selector: 'app-topbar',
 	standalone: true,
-	imports: [],
+	imports: [ToolbarModule, AvatarModule, TabViewModule],
 	templateUrl: './topbar.component.html',
 	styleUrl: './topbar.component.scss'
 })
